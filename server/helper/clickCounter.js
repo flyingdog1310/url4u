540cf02193ed7b15f846e7e39810fddd65b06402
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-04-24 19:05:46 +0800

    [Feat]Add redis & kafka helper for high demand system

--- server/helper/clickCounter.js
+++ server/helper/clickCounter.js
@@ -1,8 +1,11 @@
 import { redis } from "../../util/cache.js";
 import { kafka } from "../../util/kafka.js";
+import { insertClick, getClick } from "../../util/cassandra.js";
 
-clickConsumer("clicks", "click-counter");
+clickConsumerForRedis("clicks", "click-counter");
+//clickConsumerForCassandra("clicks", "click-counter-cassandra");
 
+//click storage for redis
 async function setClickCounter(time, clickMeta) {
   let counts;
   try {
@@ -15,7 +18,7 @@ async function setClickCounter(time, clickMeta) {
   redis.expire(time, 7200);
 }
 
-async function clickConsumer(topic, group) {
+async function clickConsumerForRedis(topic, group) {
   const consumer = kafka.consumer({ groupId: group });
   await consumer.connect();
   await consumer.subscribe({ topic: topic, fromBeginning: true });
@@ -25,9 +28,10 @@ async function clickConsumer(topic, group) {
       const clickMeta = await changeTime(clickRawMeta);
       const id = clickMeta.match(/\d+/)[0];
       const time = clickMeta.split("time:")[1].split(" referer")[0];
-      let idTime = `${id}/${time}`;
-      console.log(idTime, clickMeta);
-      setClickCounter(idTime, clickMeta);
+
+      console.log(clickMeta);
+      setClickCounter(id, clickMeta);
+      setClickCounter(time, clickMeta);
     },
   });
 }
@@ -37,8 +41,36 @@ function changeTime(clickRawMeta) {
   return clickMeta;
 }
 
-async function getClickCounter(idTime) {
-  const result = await redis.zrange(idTime, 0, -1, "WITHSCORES");
+//click storage for cassandra
+async function clickConsumerForCassandra(topic, group) {
+  const consumer = kafka.consumer({ groupId: group });
+  await consumer.connect();
+  await consumer.subscribe({ topic: topic, fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const clickRawMeta = message.value.toString();
+      const id = clickRawMeta.match(/\d+/)[0];
+      const time = clickRawMeta.split("time:")[1].split(" referer")[0];
+      const referrer = clickRawMeta.split("referer:")[1].split(" device")[0];
+      const device = clickRawMeta.split("device:")[1].split(" ip")[0];
+      const ip = clickRawMeta.split("ip:")[1].split("}")[0];
+      console.log(id, time, referrer, device, ip);
+      const result = await insertClick(id, time, referrer, device, ip);
+      console.log(result);
+    },
+  });
+}
+
+//get data from cassandra
+async function getClickFromCassandra(Time) {
+  let now = new Date();
+  now.setHours(now.getHours() - 1);
+  const start =
+    now.toISOString().slice(0, 19).replace("T", " ").split(":")[0] + ":00:00";
+  const stop =
+    now.toISOString().slice(0, 19).replace("T", " ").split(":")[0] + ":59:59";
+
+  let result = await getClick(stop, start);
   console.log(result);
   return result;
 }
---
+++ util/cassandra.js
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import cassandra from "cassandra-driver";
+
+const {
+  CASSANDRA_USER,
+  CASSANDRA_PASS,
+  CASSANDRA_CONTACT_POINT,
+  CASSANDRA_LOCAL_DATA_CENTER,
+  CASSANDRA_KEY_SPACE,
+} = process.env;
+
+const authProvider = new cassandra.auth.PlainTextAuthProvider(
+  CASSANDRA_USER,
+  CASSANDRA_PASS
+);
+export const client = new cassandra.Client({
+  contactPoints: [
+    CASSANDRA_CONTACT_POINT,
+    //process.env.CASSANDRA_CONTACT_POINT2,
+    //process.env.CASSANDRA_CONTACT_POINT3,
+  ],
+  localDataCenter: CASSANDRA_LOCAL_DATA_CENTER,
+  authProvider,
+  keyspace: CASSANDRA_KEY_SPACE,
+});
+
+const insertClick = async (urlId, time, referrer, device, region) => {
+  let params = [urlId, time, referrer, device, region];
+  const query = `INSERT INTO click (uid, urlId, time ,referrer, device ,region) VALUES (blobAsUuid(timeuuidAsBlob(now())),?,?,?,?,?);`;
+  const options = { prepare: true };
+  const result = await client.execute(query, params, options);
+  return result;
+};
+
+
+//FIXME: modify later
+const getClick = async (start, end) => {
+  let params = [start, end];
+  const query = `SELECT * FROM click WHERE time <= ? and time >= ? ALLOW FILTERING`;
+  const options = { prepare: true };
+  const result = await client.execute(query, params, options);
+  return result;
+};
+
+export { insertClick, getClick };
