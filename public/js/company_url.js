443386da0427f68c33b761c6d6fc600ee1322163
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-05-01 18:42:56 +0800

    [Feat]Add copy url button

--- public/js/company_url.js
+++ public/js/company_url.js
@@ -42,20 +42,28 @@ function renderTable(rawUrls) {
     const row = body.insertRow();
     row.innerHTML = `
       <td class='td'><a href=${url.long_url}>${url.long_url}</a><br>
-      https://url4u.today/${url.short_url}
+      <a href=${url.short_url} id="${url.short_url}-link">https://url4u.today/${url.short_url}    </a><button class="copy-button btn btn-secondary btn-xs"" data-short-url="${url.short_url}"><i class="fas fa-copy"></i></button>
       </td>
       <td><img src=https://d2zbleiceefv1c.cloudfront.net/${url.picture} height="70px" width="100px" class="display"></td>
       <td class='td'>${url.title}</td>
       <td class='td'>${url.description}</td>
       <td>${url.create_time}</td>
       <td>
-      <button type="button" class="btn btn-primary btn-xs" onclick="location.href='/url/modify/${url.id}'">Modify</button><br>
-      <button type="button" class="btn btn-primary btn-xs" onclick="location.href='/url/analytic/${url.id}'">Analytics</button>
+      <button type="button" class="btn btn-info btn-xs" onclick="location.href='/url/modify/${url.id}'"><i class="fas fa-pencil-alt"></i> Modify</button><br>
+      <button type="button" class="btn btn-primary btn-xs" onclick="location.href='/url/analytic/${url.id}'"><i class="fas fa-folder"></i> Analytics</button>
       </td>
     `;
   }
-
   document.getElementById("lists").appendChild(table);
+  $(".copy-button").on("click", function () {
+    const short_url = $(this).data("short-url");
+    const shortUrl = $(`#${short_url}-link`).text();
+    const textArea = $("<textarea>").val(shortUrl).appendTo("body");
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    alert("Copied to clipboard: " + shortUrl);
+  });
 }
 
 getCompanyUrl();
