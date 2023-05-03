f1efe250c6b02d6737badc2340c009d761baf9dd
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-05-03 15:47:54 +0800

    [Feat]Alter kafka to send batch

--- util/kafka-producer.js
+++ util/kafka-producer.js
@@ -1,5 +1,6 @@
 import { kafka } from "./kafka.js";
-const producer = kafka.producer();
+const producer = kafka.producer({ batchSize: 1000, linger: 100 });
+
 async function clickEvent(topic, value) {
   await producer.connect();
   await producer.send({
@@ -7,6 +8,9 @@ async function clickEvent(topic, value) {
     messages: [{ value: value }],
   });
 }
+setInterval(async () => {
+  await producer.sendBatch();
+}, 1000);
 
 await producer.disconnect();
 export { clickEvent };
