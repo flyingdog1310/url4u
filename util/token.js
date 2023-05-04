8549d2d5f0a63cc401e1fe92fa760f8be7c82f27
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-05-04 10:54:31 +0800

    [Feat]Add variable for jwt time

--- util/token.js
+++ util/token.js
@@ -1,3 +1,7 @@
+import path from "path";
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+import dotenv from "dotenv";
+dotenv.config({ path: __dirname +"/../.env" });
 import jwt from "jsonwebtoken";
 
 const { JWT_SHORT_EXPIRE, JWT_LONG_EXPIRE, JWT_SECRET } = process.env;
