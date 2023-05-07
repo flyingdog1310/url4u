bd1ac437edc3f84473487a0b7ba457b983e88763
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-05-08 01:06:45 +0800

    [Feat]Modify frontend button

--- server/helper/fakeDataGenerator.js
+++ server/helper/fakeDataGenerator.js
@@ -1,14 +1,43 @@
 import { createClick } from "../models/ad_model.js";
 
-let deviceArr = ["Macintosh", "Windows", "iPhone", "Android", "iPad"];
-let ipArr = ["TW", "US", "JP", "IN"];
-let idArr = [1, 2, 3];
+let deviceArr = [
+  "Macintosh",
+  "Macintosh",
+  "Macintosh",
+  "Macintosh",
+  "Macintosh",
+  "Macintosh",
+  "Macintosh",
+  "Windows",
+  "Windows",
+  "Windows",
+  "iPhone",
+  "Android",
+  "iPad",
+];
+let ipArr = [
+  "TW",
+  //, "US", "JP", "IN"
+];
+let idArr = [1];
 let referrerArr = [
   "https://www.google.com",
   "https://www.youtube.com/",
   "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
+  "https://l.facebook.com/",
   "https://m.facebook.com/",
   "https://l.instagram.com/",
+  "https://m.instagram.com/",
+  "https://m.instagram.com/",
+  "https://m.instagram.com/",
+  "https://m.instagram.com/",
+  "https://m.instagram.com/",
   "https://m.instagram.com/",
 ];
 
@@ -25,17 +54,27 @@ for (let i = 0; i < range; i++) {
   now.setHours(now.getHours() - 1);
 }
 
-
 for (let i = 0; i < timeArr.length; i++) {
-  for (let j = 0; j < 10; j++) {
-    let count = mathRandom(1,1000)
+  
+    let count = 0;
+    if (timeArr[i].split(" ")[1].slice(0, 2)>= 0 && timeArr[i].split(" ")[1].slice(0, 2) < 11) {
+      count = mathRandom(20, 80);
+    } else if (
+      timeArr[i].split(" ")[1].slice(0, 2) >= 11 &&
+      timeArr[i].split(" ")[1].slice(0, 2) < 16
+    ) {
+      count = mathRandom(50, 150);
+    } else {
+      count = mathRandom(1, 50);
+    }
+
     let id = randomArr(idArr);
     let time = timeArr[i];
     let referrer = randomArr(referrerArr);
     let device = randomArr(deviceArr);
     let ip = randomArr(ipArr);
     await createClick(id, time, referrer, device, ip, count);
-  }
+  
 }
 
 function randomArr(arr) {
