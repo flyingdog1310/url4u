0b71f5b9ffb7172f61355129be7a7c3f3ef1a511
flyingdog1310 <116939147+flyingdog1310@users.noreply.github.com>
2023-05-12 14:31:13 +0800

    [Feat]Add authorization for company API

--- server/controllers/api_company_controller.js
+++ server/controllers/api_company_controller.js
@@ -7,6 +7,10 @@ import {
   getUsersByCompany,
 } from "../models/company_model.js";
 import { userSignIn } from "../models/user_model.js";
+import {
+  getRoleByUserCompany,
+  getRoleByUserUrl,
+} from "../models/user_model.js";
 
 const createUserCompany = async (req, res) => {
   const { company_name } = req.body;
@@ -19,6 +23,10 @@ const createCompanyUser = async (req, res) => {
   const { user_role, user_email } = req.body;
   const user_id = res.locals.decoded.userId;
   const company_id = req.originalUrl.split("/")[4];
+  const isAuthorized = await getRoleByUserCompany(user_id, company_id);
+  if (isAuthorized.user_role != 0) {
+    return res.status(403).json("Only Admin can add member");
+  }
   const newUser = await userSignIn(user_email);
   if (!newUser) {
     return res.status(400).json("No user with such email");
@@ -35,20 +43,34 @@ const createCompanyUser = async (req, res) => {
 };
 
 const getShortUrlListByCompany = async (req, res) => {
+  const user_id = res.locals.decoded.userId;
   const company_id = req.originalUrl.split("/")[4];
+  const isAuthorized = await getRoleByUserCompany(user_id, company_id);
+  if (isAuthorized) {
+    return res.status(403).json("Unauthorized");
+  }
   const url = await getUrlsByCompany(company_id);
   return res.status(200).json(url);
 };
 
 const getShortUrlListByUrl = async (req, res) => {
+  const user_id = res.locals.decoded.userId;
   const url_id = req.originalUrl.split("/")[5];
-  console.log(url_id);
+  const isAuthorized = await getRoleByUserUrl(user_id, url_id);
+  if (!isAuthorized) {
+    return res.status(403).json("Unauthorized");
+  }
   const url = await getUrlsByUrl(url_id);
   return res.status(200).json(url);
 };
 
 const getCompanyUser = async (req, res) => {
+  const user_id = res.locals.decoded.userId;
   const company_id = req.originalUrl.split("/")[4];
+  const isAuthorized = await getRoleByUserCompany(user_id, company_id);
+  if (!isAuthorized) {
+    return res.status(403).json("Unauthorized");
+  }
   const user = await getUsersByCompany(company_id);
   return res.status(200).json(user);
 };
